6fbb1f56ad00c55c445b0bea56bdf16da7aa6287
Jonathan Martin <JimboOmega@gmail.com>
2014-07-20 15:03:34 -0700

    Controlled Noms
    Sets a storage variable so that it will not automatically whisk you away
    on the path to purchasing every time you view a normal page.

--- keep_nomming.js
+++ keep_nomming.js
@@ -8,7 +8,16 @@ function click_on_preferred_size(preferredSize)
   
 }
 
-$(function(){
+function complete_purchase_and_stop_nomming(){
+  chrome.storage.sync.set({
+    goingCrazy: false
+  })
+  jQuery("#paypal-payments").find('input').click()
+  jQuery("#complete-purchase").click()
+
+}
+
+function go_forth_and_buy(){
   //pick first search result (if the search page)
   $('.productfirst img').click()
   //if this is the size selection page, pick the user's size and add to cart
@@ -28,8 +37,17 @@ $(function(){
   //clicks "continue to next step"
   $('#commit-button').click()
   $('#paypal_express').click()
-  jQuery("#paypal-payments").waitUntilExists(function(){
-    jQuery("#paypal-payments").find('input').click()
-	jQuery("#complete-purchase").click()
+  jQuery("#paypal-payments").waitUntilExists(complete_purchase_and_stop_nomming)
+}
+
+$(function(){
+  chrome.storage.sync.get({
+      goingCrazy: false
+  },
+  function(storage_results){
+    if(storage_results.goingCrazy)
+	{
+      go_forth_and_buy()
+	}
   })
 })
--- search_and_buy.js
+++ search_and_buy.js
@@ -1,8 +1,16 @@
 $("#buy-button").click(function() {
   searchString = $("#search-input").val()
-  searchURL = "http://blackmilkclothing.com/search?type=product&q=" + searchString
+  searchURL = "http://blackmilkclothing.com/search?type=product&q=" + searchString 
+  switch_tab = function(){
+	  console.log(searchURL)
+	  chrome.tabs.getSelected(null, function(tab){
+        chrome.tabs.update(tab.id, {url: searchURL});	  
+      })
+	}
+  chrome.storage.sync.set({
+    goingCrazy: true
+	}
+  )
+  switch_tab()
   
-  chrome.tabs.getSelected(null, function(tab){
-    chrome.tabs.update(tab.id, {url: searchURL});
-  })
 })
